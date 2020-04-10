e5e978fae34294f8dbb1763f0a23ae7ab9573426
summer <2515011987@qq.com>
2020-04-10 21:21:31 +0800

    展示首页

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 引入css文件
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
 new Vue({
   render: h => h(App),
 }).$mount('#app')
