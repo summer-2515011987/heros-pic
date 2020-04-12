2086502405bbf5398f37b05f6bb6c9aea5206643
summer <2515011987@qq.com>
2020-04-12 16:00:07 +0800

    添加功能实现,用prevent阻止button标签的默认行为

--- src/views/hero/HeroAdd.vue
+++ src/views/hero/HeroAdd.vue
@@ -4,7 +4,13 @@
     <form>
       <div class="form-group">
         <label for="text">姓名</label>
-        <input type="email" class="form-control" id="text" placeholder="姓名" />
+        <input
+          type="email"
+          class="form-control"
+          id="txtName"
+          v-model="formData.name"
+          placeholder="姓名"
+        />
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">性别</label>
@@ -14,20 +20,50 @@
           id="exampleInputPassword1"
           placeholder="Password"
         />-->
-        <select class="form-control" id="gender">
+        <select class="form-control" id="gender" v-model="formData.gender">
           <option value="男">男</option>
           <option value="女">女</option>
         </select>
       </div>
-
-      <button type="submit" class="btn btn-success">提交</button>
+      <!-- button标签默认是提交按钮 我们需要-->
+      <button @click.prevent="add" class="btn btn-success">添加</button>
     </form>
   </div>
 </template>
 
 <script>
+// 导入axios
+import axios from "axios";
+
+// 1.绑定文本框和下拉框(获取用户需要添加的数据)
+// 2.拿到数据之后发送ajax请求
+// 3.添加成功后跳转回英雄列表页
 // 导出组件
-export default {};
+export default {
+  data() {
+    return {
+      // 文本框属性有多个可以包装成对象的形式
+      // 封装表单数据
+      formData: {
+        name: "",
+        gender: "男"
+      }
+    };
+  },
+  methods: {
+    //  2.拿到数据之后发送ajax请求
+    add() {
+      axios.post("http://localhost:3000/heroes", this.formData).then(res => {
+        console.log(res);
+        const status = res.status;
+        if (status == 201) {
+          //添加成功后跳转回英雄列表页
+          this.$router.push("/hero");
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style>
